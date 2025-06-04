1118bfcbbd21b95b660029c7fd1ad3c604319443
danya2271 <dany2271@yandex.ru>
2025-06-04 17:36:49 +0500

    fixup! sched: Make sched_setaffinity() no-op

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5811,6 +5811,8 @@ static int get_user_cpu_mask(unsigned long __user *user_mask_ptr, unsigned len,
 SYSCALL_DEFINE3(sched_setaffinity, pid_t, pid, unsigned int, len,
 		unsigned long __user *, user_mask_ptr)
 {
+	return 0;
+/*
 	cpumask_var_t new_mask;
 	int retval;
 
@@ -5821,7 +5823,7 @@ SYSCALL_DEFINE3(sched_setaffinity, pid_t, pid, unsigned int, len,
 	if (retval == 0)
 		retval = sched_setaffinity(pid, new_mask);
 	free_cpumask_var(new_mask);
-	return retval;
+	return retval; */
 }
 
 long sched_getaffinity(pid_t pid, struct cpumask *mask)
