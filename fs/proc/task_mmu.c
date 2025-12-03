1fa0c6639c2c44c92e1170cf8f991bfb24bc9a63
zyexro <zyexro@proton.me>
2025-12-03 15:26:58 +0100

    feat: fs: susfs v1.5.5@001e6991
    from: https://gitlab.com/simonpunk/susfs4ksu.git
    
    Co-authored-by: simonpunk <simonpunk2016@gmail.com>
    Signed-off-by: zyexro <zyexro@proton.me>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -20,6 +20,9 @@
 #include <linux/uaccess.h>
 #include <linux/mm_inline.h>
 #include <linux/pkeys.h>
+#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
+#include <linux/susfs_def.h>                                                                       
+#endif
 
 #include <asm/elf.h>
 #include <asm/tlb.h>
@@ -490,6 +493,27 @@ static int show_vma_header_prefix(struct seq_file *m, unsigned long start,
 	return 0;
 }
 
+static void show_vma_header_prefix_fake(struct seq_file *m,
+				   unsigned long start, unsigned long end,
+				   vm_flags_t flags, unsigned long long pgoff,
+				   dev_t dev, unsigned long ino)
+{
+	seq_setwidth(m, 25 + sizeof(void *) * 6 - 1);
+	seq_printf(m, "%08lx-%08lx %c%c%c%c %08llx %02x:%02x %lu ",
+		    start,
+		    end,
+		    flags & VM_READ ? 'r' : '-',
+		    flags & VM_WRITE ? '-' : '-',
+		    flags & VM_EXEC ? '-' : '-',
+		    flags & VM_MAYSHARE ? 's' : 'p',
+		    pgoff,
+		    MAJOR(dev), MINOR(dev), ino);
+}
+
+#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
+extern void susfs_sus_ino_for_show_map_vma(unsigned long ino, dev_t *out_dev, unsigned long *out_ino);
+#endif
+
 static void
 show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
 {
@@ -504,8 +528,17 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
 
 	if (file) {
 		struct inode *inode = file_inode(vma->vm_file);
+#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
+		if (unlikely(inode->i_state & INODE_STATE_SUS_KSTAT)) {
+			susfs_sus_ino_for_show_map_vma(inode->i_ino, &dev, &ino);
+			goto bypass_orig_flow;
+		}
+#endif
 		dev = inode->i_sb->s_dev;
 		ino = inode->i_ino;
+#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
+bypass_orig_flow:
+#endif
 		pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
 	}
 
